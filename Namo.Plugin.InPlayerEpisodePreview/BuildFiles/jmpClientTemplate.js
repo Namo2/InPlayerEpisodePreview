6fffe87c434d1b964fd462e8113248ac8b3067e4
Namo <namo_2@gmx.de>
2024-02-20 16:28:20 +0100

    Added a batch file which can produce and bundle the code as different release versions

---
+++ Namo.Plugin.InPlayerEpisodePreview/BuildFiles/jmpClientTemplate.js
@@ -0,0 +1,22 @@
+class inPlayerEpisodePreviewPlugin {
+    constructor({ playbackManager, events, ServerConnections }) {
+        this.name = 'InPlayerEpisodePreview Plugin';
+        this.type = 'input';
+        this.id = 'inPlayerEpisodePreviewPlugin';
+
+        (async () => {
+            await window.initCompleted;
+            const enabled = window.jmpInfo.settings.plugins.inPlayerEpisodePreviewPlugin;
+
+            console.log("InPlayerEpisodePreview Plugin enabled: " + enabled);
+            // if (!enabled) return;
+
+            // InPlayerEpisodePreview Web-Client Code Start
+
+            JMP_CLIENT_CODE
+            // InPlayerEpisodePreview Web-Client Code End
+        })();
+    }
+}
+
+window._inPlayerEpisodePreviewPlugin = inPlayerEpisodePreviewPlugin;

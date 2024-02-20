d86bc975b2cf85eee5e9be6d4fa11a714eb1eed8
Namo <namo_2@gmx.de>
2024-02-20 16:26:25 +0100

    Implemented isJMPClient flag via an environment variable

--- Namo.Plugin.InPlayerEpisodePreview/webpack.config.js
+++ Namo.Plugin.InPlayerEpisodePreview/webpack.config.js
@@ -1,21 +1,31 @@
-﻿module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: "./Web/inPlayerPreview.ts",
-    output: {
-        path: __dirname + "/Web",
-        filename: "inPlayerPreview.js"
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader"
-            }
-        ]
+﻿const webpack = require('webpack');
+
+module.exports = (env) => {
+    console.log("CLIENT: " + env.CLIENT)
+    
+    return {
+        mode: "development",
+        devtool: "inline-source-map",
+        entry: "./Web/inPlayerPreview.ts",
+        plugins: [
+            new webpack.DefinePlugin({
+                CLIENT: JSON.stringify(env.CLIENT ??= false),
+            })
+        ],
+        output: {
+            path: __dirname + "/Web",
+            filename: "inPlayerPreview.js"
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader"
+                }
+            ]
+        }
     }
 };
-

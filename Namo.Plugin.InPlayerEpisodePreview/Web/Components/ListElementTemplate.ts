c7f37c4789618fc9ed7f385967dc0ac51e6763b0
Namo <Namo_2@gmx.de>
2025-11-06 09:20:55 +0100

    chore: don't show play button for the current episode

--- Namo.Plugin.InPlayerEpisodePreview/Web/Components/ListElementTemplate.ts
+++ Namo.Plugin.InPlayerEpisodePreview/Web/Components/ListElementTemplate.ts
@@ -11,31 +11,31 @@ export class ListElementTemplate extends BaseTemplate {
     private readonly quickActionContainer: HTMLElement
     private playStateIcon: PlayStateIconTemplate
     private favoriteIcon: FavoriteIconTemplate
-    
+
     constructor(container: HTMLElement, positionAfterIndex: number, private item: BaseItem, private playbackHandler: PlaybackHandler, private programDataStore: ProgramDataStore) {
         super(container, positionAfterIndex)
         this.setElementId(`episode-${item.IndexNumber}`)
 
         // create temp quick action container
         this.quickActionContainer = document.createElement('div')
-        
+
         // create quick actions
         this.playStateIcon = new PlayStateIconTemplate(this.quickActionContainer, -1, this.item)
         this.favoriteIcon = new FavoriteIconTemplate(this.quickActionContainer, 0, this.item)
     }
-    
+
     getTemplate(): string {
         // add quick actions
         this.playStateIcon.render()
         this.favoriteIcon.render()
-        
+
         // add episode details/info
         const detailsContainer: HTMLDivElement = document.createElement('div')
         const details: EpisodeDetailsTemplate = new EpisodeDetailsTemplate(detailsContainer, -1, this.item)
         details.render()
-        
+
         const backgroundImageStyle: string = `background-image: url('../Items/${this.item.Id}/Images/Primary?tag=${this.item.ImageTags.Primary}')`
-        
+
         // language=HTML
         return `
             <div id="${this.getElementId()}"
@@ -45,8 +45,8 @@ export class ListElementTemplate extends BaseTemplate {
                 <div class="previewEpisodeContainer flex">
                     <button class="listItem previewEpisodeTitle" type="button">
                         ${(
-                            this.item.IndexNumber && 
-                            this.programDataStore.type !== ItemType.Movie
+                                this.item.IndexNumber &&
+                                this.programDataStore.type !== ItemType.Movie
                         ) ? `<span>${this.item.IndexNumber}</span>` : ''}
                         <div class="listItemBody actionsheetListItemBody">
                             <span class="actionSheetItemText">${this.item.Name}</span>
@@ -79,16 +79,18 @@ export class ListElementTemplate extends BaseTemplate {
                                             </div>
                                         </div>
                                     </button>
-                                    <div class="cardOverlayContainer itemAction"
+                                    ${this.item.Id !== this.programDataStore.activeMediaSourceId && 
+                                    `<div class="cardOverlayContainer itemAction"
                                          data-action="link">
                                         <button id="start-episode-${this.item.IndexNumber}"
                                                 is="paper-icon-button-light"
                                                 class="cardOverlayButton cardOverlayButton-hover itemAction paper-icon-button-light cardOverlayFab-primary"
                                                 data-action="resume">
-                                    <span class="material-icons cardOverlayButtonIcon cardOverlayButtonIcon-hover play_arrow"
-                                          aria-hidden="true"></span>
+                                            <span class="material-icons cardOverlayButtonIcon cardOverlayButtonIcon-hover play_arrow"
+                                                aria-hidden="true"/>
                                         </button>
-                                    </div>
+                                    </div>`
+                                    }
                                 </div>
                             </div>
                         </div>
@@ -104,7 +106,7 @@ export class ListElementTemplate extends BaseTemplate {
     public render(clickHandler: Function): void {
         let renderedElement: HTMLElement = this.addElementToContainer()
         renderedElement.addEventListener('click', (e) => clickHandler(e))
-        
+
         // add event handler to start the playback of this episode
         let episodeImageCard: HTMLElement = document.getElementById(`start-episode-${this.item.IndexNumber}`)
         episodeImageCard.addEventListener('click', () => this.playbackHandler.play(this.item.Id, this.item.UserData.PlaybackPositionTicks))

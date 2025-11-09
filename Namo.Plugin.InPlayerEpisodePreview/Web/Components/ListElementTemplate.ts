8af6da7318ea83a4988191d4d010937b0bb18a90
Namo <48796374+Namo2@users.noreply.github.com>
2025-11-09 20:43:36 +0100

    feat: plugin settings (#70)
    * feat: plugin settings
    * chore: don't show play button for the current episode

--- Namo.Plugin.InPlayerEpisodePreview/Web/Components/ListElementTemplate.ts
+++ Namo.Plugin.InPlayerEpisodePreview/Web/Components/ListElementTemplate.ts
@@ -11,31 +11,31 @@ export class ListElementTemplate extends BaseTemplate {
     private readonly quickActionContainer: HTMLElement
     private playStateIcon: PlayStateIconTemplate
     private favoriteIcon: FavoriteIconTemplate
-    
+
     constructor(container: HTMLElement, positionAfterIndex: number, private item: BaseItem, private playbackHandler: PlaybackHandler, private programDataStore: ProgramDataStore) {
         super(container, positionAfterIndex)
         this.setElementId(`episode-${item.IndexNumber}`)
 
         // create temp quick action container
         this.quickActionContainer = document.createElement('div')
-        
+
         // create quick actions
         this.playStateIcon = new PlayStateIconTemplate(this.quickActionContainer, -1, this.item)
         this.favoriteIcon = new FavoriteIconTemplate(this.quickActionContainer, 0, this.item)
     }
-    
+
     getTemplate(): string {
         // add quick actions
         this.playStateIcon.render()
         this.favoriteIcon.render()
-        
+
         // add episode details/info
         const detailsContainer: HTMLDivElement = document.createElement('div')
         const details: EpisodeDetailsTemplate = new EpisodeDetailsTemplate(detailsContainer, -1, this.item)
         details.render()
-        
+
         const backgroundImageStyle: string = `background-image: url('../Items/${this.item.Id}/Images/Primary?tag=${this.item.ImageTags.Primary}')`
-        
+
         // language=HTML
         return `
             <div id="${this.getElementId()}"
@@ -45,8 +45,8 @@ export class ListElementTemplate extends BaseTemplate {
                 <div class="previewEpisodeContainer flex">
                     <button class="listItem previewEpisodeTitle" type="button">
                         ${(
-                            this.item.IndexNumber && 
-                            this.programDataStore.type !== ItemType.Movie
+                                this.item.IndexNumber &&
+                                this.programDataStore.type !== ItemType.Movie
                         ) ? `<span>${this.item.IndexNumber}</span>` : ''}
                         <div class="listItemBody actionsheetListItemBody">
                             <span class="actionSheetItemText">${this.item.Name}</span>
@@ -66,33 +66,36 @@ export class ListElementTemplate extends BaseTemplate {
                                     <div class="cardPadder cardPadder-overflowBackdrop lazy-hidden-children">
                                         <span class="cardImageIcon material-icons tv" aria-hidden="true"/>
                                     </div>
-                                    <canvas aria-hidden="true" width="20" height="20"
-                                            class="blurhash-canvas lazy-hidden"></canvas>
                                     <button id="previewEpisodeImageCard-${this.item.IndexNumber}"
-                                            class="cardImageContainer cardContent itemAction lazy blurhashed lazy-image-fadein-fast"
+                                            class="cardImageContainer cardContent itemAction lazy blurhashed lazy-image-fadein-fast ${this.programDataStore.settings.BlurDescription ? 'blur' : ''}"
                                             data-action="link"
                                             style="${backgroundImageStyle}">
-                                        <div class="innerCardFooter fullInnerCardFooter innerCardFooterClear ${this.item.UserData.PlayedPercentage ? '' : 'hide'}">
-                                            <div class="itemProgressBar">
-                                                <div class="itemProgressBarForeground"
-                                                     style="width:${this.item.UserData.PlayedPercentage}%;"></div>
-                                            </div>
-                                        </div>
                                     </button>
-                                    <div class="cardOverlayContainer itemAction"
-                                         data-action="link">
-                                        <button id="start-episode-${this.item.IndexNumber}"
-                                                is="paper-icon-button-light"
-                                                class="cardOverlayButton cardOverlayButton-hover itemAction paper-icon-button-light cardOverlayFab-primary"
-                                                data-action="resume">
-                                    <span class="material-icons cardOverlayButtonIcon cardOverlayButtonIcon-hover play_arrow"
-                                          aria-hidden="true"></span>
-                                        </button>
-                                    </div>
+                                    ${this.item.UserData.PlayedPercentage ? 
+                                        `<div class="innerCardFooter fullInnerCardFooter innerCardFooterClear itemProgressBar">
+                                            <div class="itemProgressBarForeground"
+                                                style="width:${this.item.UserData.PlayedPercentage}%;">           
+                                            </div>
+                                        </div>` : ''
+                                    }
+                                    ${this.item.Id !== this.programDataStore.activeMediaSourceId ? 
+                                        `<div class="cardOverlayContainer itemAction"
+                                             data-action="link">
+                                            <button id="start-episode-${this.item.IndexNumber}"
+                                                    is="paper-icon-button-light"
+                                                    class="cardOverlayButton cardOverlayButton-hover itemAction paper-icon-button-light cardOverlayFab-primary"
+                                                    data-action="resume">
+                                                <span class="material-icons cardOverlayButtonIcon cardOverlayButtonIcon-hover play_arrow"
+                                                    aria-hidden="true"/>
+                                            </button>
+                                        </div>` : ''
+                                    }
                                 </div>
                             </div>
                         </div>
-                        <span class="previewEpisodeDescription">${this.item.Description ?? 'loading...'}</span>
+                        <span class="previewEpisodeDescription ${this.programDataStore.settings.BlurDescription ? 'blur' : ''}">
+                            ${this.item.Description ?? 'loading...'}
+                        </span>
                     </div>
                 </div>
             </div>
@@ -100,11 +103,13 @@ export class ListElementTemplate extends BaseTemplate {
     }
 
     public render(clickHandler: Function): void {
-        let renderedElement: HTMLElement = this.addElementToContainer()
+        const renderedElement: HTMLElement = this.addElementToContainer()
         renderedElement.addEventListener('click', (e) => clickHandler(e))
-        
-        // add event handler to start the playback of this episode
-        let episodeImageCard: HTMLElement = document.getElementById(`start-episode-${this.item.IndexNumber}`)
-        episodeImageCard.addEventListener('click', () => this.playbackHandler.play(this.item.Id, this.item.UserData.PlaybackPositionTicks))
+
+        if (this.item.Id !== this.programDataStore.activeMediaSourceId) {
+            // add event handler to start the playback of this episode
+            const episodeImageCard: HTMLElement = document.getElementById(`start-episode-${this.item.IndexNumber}`)
+            episodeImageCard.addEventListener('click', () => this.playbackHandler.play(this.item.Id, this.item.UserData.PlaybackPositionTicks))
+        }
     }
 }

af10ee474e8276def21067363958203c24295cca
Namo <namo_2@gmx.de>
2026-09-09 20:24:47 +0200

    refactor: extract inline CSS into a dedicated file, bundled via style-loader

--- Namo.Plugin.InPlayerEpisodePreview/webpack.config.js
+++ Namo.Plugin.InPlayerEpisodePreview/webpack.config.js
@@ -20,6 +20,10 @@ module.exports = () => {
                 {
                     test: /\.tsx?$/,
                     loader: "ts-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"]
                 }
             ]
         }
